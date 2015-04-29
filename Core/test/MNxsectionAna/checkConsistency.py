aa9d98a64f2abdd1f78e7ac68679032e64c6f232
Tomasz <fruboes@cern.ch>
2015-04-29 09:42:04 +0200

    Consistency checks

---
+++ Core/test/MNxsectionAna/checkConsistency.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+import ROOT
+ROOT.gROOT.SetBatch(True)
+
+import glob
+import sys
+
+sample2type2cnt = {}
+
+for f in glob.glob("*.root"):
+    if f.count("_") == 1 : continue
+    spl = f.split("_")
+    anaType = spl.pop(1)
+    spl.pop(0)
+    sample = "_".join(spl).replace(".root", "")
+
+    sample2type2cnt.setdefault(sample, {})
+    try:
+        infile = ROOT.TFile(f) 
+        histSkim = infile.Get(sample+"/CFFEventsSeenSkim")
+        histProof = infile.Get(sample+"/CFFEventsSeenProof")
+        #print anaType, sample, hist.GetBinContent(1)
+        cnt = histProof.GetBinContent(1)
+        cntFromSkim = histSkim.GetBinContent(2)
+        if cnt != cntFromSkim:
+            print "Warning - different events seen by tree producer and py analyzer:", cntFromSkim, cnt, "(r =", cntFromSkim/cnt, ")"
+            print "   (",sample, anaType,  ")"
+
+        
+        sample2type2cnt[sample][anaType]=cnt
+    except:
+        print "Some problem with", anaType, sample
+
+
+allAnaTypes = set(reduce(list.__add__, [sample2type2cnt[s].keys() for s in sample2type2cnt]))
+#print allAnaTypes
+
+for s in sample2type2cnt:
+    typesForThisSample = set(sample2type2cnt[s].keys())
+    if len(allAnaTypes-typesForThisSample) != 0:
+        print "Warning - missing ana for", s, "-", " ".join(allAnaTypes-typesForThisSample)
+
+    cnts = set([sample2type2cnt[s][t] for t in sample2type2cnt[s]])
+    if len(cnts)>1 or len(cnts)==1 and list(cnts)[0]==0:
+        print "Problem: ", s, " ".join(map(str,cnts))
+
+
