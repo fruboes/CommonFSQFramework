8baa1897e8fb94d7d5542a8b25b02bf1d07dad22
Tomasz <fruboes@cern.ch>
2015-04-29 09:42:35 +0200

    pbs submit helper script

---
+++ Core/test/MNxsectionAna/submit.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "./do.py -c MNWindow" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
+echo "./do.py -c MNBasic" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
+echo "./do.py -c MNAsym" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
+echo "./do.py -c InclusiveWindow" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
+echo "./do.py -c InclusiveBasic" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
+echo "./do.py -c InclusiveAsym" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
+#echo "./do.py -c InclusiveAsym,InclusiveWindow,InclusiveBasic,MNBasic,MNAsym" | qsub  -V -l nodes=1:ppn=40 -l mem=160GB -q local -d `pwd`
